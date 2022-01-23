e342728029367c0f02da8c139d5eee7dda6e6f8c
AmmarHazem <ammar.doctorsae@outlook.com>
2022-01-23 17:57:58 +0400

    Add ip router and rate limitter config

--- app.js
+++ app.js
@@ -15,7 +15,15 @@ const { response } = require("express");
 
 const app = express();
 app.set("trust proxy", 1);
-app.use(rateLimiter({ windowMs: 15 * 60 * 1000 }));
+app.get("/ip", (request, response) => response.send(request.ip));
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    store: new MemoryStore(),
+  })
+);
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
