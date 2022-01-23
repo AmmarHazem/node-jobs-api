04e1f4a766ba637b9887ae6e854830fbe04c9c39
AmmarHazem <ammar.doctorsae@outlook.com>
2022-01-23 18:00:37 +0400

    remove store from rate limiter config

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use(
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    store: new MemoryStore(),
   })
 );
 app.use(express.json());
